eff5be18f0192e42a5c9495b9c6e87caa1b28717
J-Fi <y.neczek@gmail.com>
2020-04-20 00:41:11 +0200

    add FilmRepository interface

---
+++ src/main/java/com/janflpk/collectionsmanager/backend/repository/FilmRepository.java
@@ -0,0 +1,19 @@
+package com.janflpk.collectionsmanager.backend.repository;
+
+import com.janflpk.collectionsmanager.backend.domain.Film;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+
+@Transactional
+@Repository
+public interface FilmRepository extends CrudRepository<Film, Long> {
+    @Override
+    Optional<Film> findById(Long id);
+
+    @Override
+    List<Film> findAll();
+}
