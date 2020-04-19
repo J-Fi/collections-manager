01b52ecda0ef52dd2bfdd287294bc7995e1cad86
J-Fi <y.neczek@gmail.com>
2020-04-19 15:48:36 +0200

    initial commit

---
+++ src/main/java/com/janflpk/booksmanager/BooksManagerApplication.java
@@ -0,0 +1,13 @@
+package com.janflpk.booksmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BooksManagerApplication.class, args);
+    }
+
+}
