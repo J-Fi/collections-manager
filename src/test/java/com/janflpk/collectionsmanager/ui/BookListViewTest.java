6b32d396b971e33297ca52f8f1c80856ff82442e
J-Fi <y.neczek@gmail.com>
2020-05-19 01:14:51 +0200

    add unit tests for getBookWithIsn() of BookListView class

--- src/test/java/com/janflpk/collectionsmanager/ui/BookListViewTest.java
+++ src/test/java/com/janflpk/collectionsmanager/ui/BookListViewTest.java
@@ -1,4 +1,46 @@
 package com.janflpk.collectionsmanager.ui;
 
+import com.janflpk.collectionsmanager.backend.domain.books.Book;
+import com.janflpk.collectionsmanager.ui.view.BookListView;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class BookListViewTest {
+
+    BookListView bookListView = new BookListView();
+
+    @Test
+    public void shouldFetchBookWithIsbn10Test() {
+        //Given & When
+        Book bookReturned = bookListView.getBookWithIsbn("1234567890");
+
+        //Then
+        Assert.assertNotNull(bookReturned);
+        Assert.assertEquals("1234567890", bookReturned.getIsbn());
+        Assert.assertNull(bookReturned.getIsbn13());
+        }
+
+    @Test
+    public void shouldFetchBookWithIsbn13Test() {
+        //Given & When
+        Book bookReturned = bookListView.getBookWithIsbn("1234567891011");
+
+        //Then
+        Assert.assertNull(bookReturned.getIsbn());
+        Assert.assertEquals("1234567891011", bookReturned.getIsbn13());
+    }
+
+    @Test
+    public void shouldFetchBookWithIsbnNoneTest() {
+        //Given & When
+        Book bookReturned = bookListView.getBookWithIsbn("1");
+
+        //Then
+        Assert.assertEquals(new Book(), bookReturned);
+    }
 }
