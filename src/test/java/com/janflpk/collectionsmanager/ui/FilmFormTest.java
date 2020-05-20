df259e4188e348daed376d3ee1ff4732381190df
J-Fi <y.neczek@gmail.com>
2020-05-20 23:41:11 +0200

    add unit test of FilmForm

--- src/test/java/com/janflpk/collectionsmanager/ui/FilmFormTest.java
+++ src/test/java/com/janflpk/collectionsmanager/ui/FilmFormTest.java
@@ -1,6 +1,8 @@
-/*
 package com.janflpk.collectionsmanager.ui;
 
+import com.janflpk.collectionsmanager.ui.view.FilmForm;
+import org.junit.Assert;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -9,5 +11,16 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class FilmFormTest {
 
+    private FilmForm filmForm = new FilmForm();
+
+    @Test
+    public void shouldInitiateBinder() {
+        //Given & When
+        boolean isBinderValid_1 = filmForm.getBinder().isValid();
+        boolean isBinderValid_2 = filmForm.getBinder().validate().isOk();
+
+        //Then
+        Assert.assertTrue(isBinderValid_1);
+        Assert.assertTrue(isBinderValid_2);
+    }
 }
-*/
