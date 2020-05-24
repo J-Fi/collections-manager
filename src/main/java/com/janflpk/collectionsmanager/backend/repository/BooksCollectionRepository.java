cafe4e7d834a0a845995bef79a01e48b9d3299fc
J-Fi <y.neczek@gmail.com>
2020-05-24 20:09:44 +0200

    add unit tests for BooksCollectionRepository class

--- src/main/java/com/janflpk/collectionsmanager/backend/repository/BooksCollectionRepository.java
+++ src/main/java/com/janflpk/collectionsmanager/backend/repository/BooksCollectionRepository.java
@@ -4,8 +4,10 @@ import com.janflpk.collectionsmanager.backend.domain.books.BooksCollection;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
+@Transactional
 @Repository
 public interface BooksCollectionRepository extends CrudRepository<BooksCollection, Long> {
 
---
+++ src/test/java/com/janflpk/collectionsmanager/repository/BooksCollectionRepositoryTest.java
@@ -0,0 +1,35 @@
+package com.janflpk.collectionsmanager.repository;
+
+import com.janflpk.collectionsmanager.backend.domain.books.BooksCollection;
+import com.janflpk.collectionsmanager.backend.repository.BooksCollectionRepository;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BooksCollectionRepositoryTest {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(BooksCollectionRepositoryTest.class);
+
+    @Autowired
+    private BooksCollectionRepository booksCollectionRepository;
+
+    @Test
+    public void shouldFetchAllBooksCollectionTest() {
+        //Given & When
+        List<BooksCollection> allBooksCollections = booksCollectionRepository.findAll();
+
+        //Then
+        LOGGER.info("Liczba kolekcji książek = " + allBooksCollections.size());
+        Assert.assertNotNull(allBooksCollections);
+        Assert.assertNotEquals(0, allBooksCollections);
+    }
+}
