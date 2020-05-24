345dbdfb0809e66ebfd5f2f6e5c4d8b512d887b6
J-Fi <y.neczek@gmail.com>
2020-05-24 23:18:34 +0200

    add finAll() method to FilmsCollectionDbService class

--- src/main/java/com/janflpk/collectionsmanager/backend/service/FilmsCollectionDbService.java
+++ src/main/java/com/janflpk/collectionsmanager/backend/service/FilmsCollectionDbService.java
@@ -5,6 +5,8 @@ import com.janflpk.collectionsmanager.backend.repository.FilmsCollectionReposito
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class FilmsCollectionDbService {
     @Autowired
@@ -20,5 +22,9 @@ public class FilmsCollectionDbService {
 
     public FilmsCollection findById(final Long id) {
         return filmsCollectionRepo.findById(id).orElse(new FilmsCollection());
+    }
+
+    public List<FilmsCollection> findAll() {
+        return filmsCollectionRepo.findAll();
     }
 }
