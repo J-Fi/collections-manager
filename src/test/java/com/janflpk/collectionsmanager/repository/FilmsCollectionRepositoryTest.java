a90a6332fa09c55e09d4456d8402de89a2a35e48
J-Fi <y.neczek@gmail.com>
2020-05-24 20:16:24 +0200

    add unit tests for FilmsCollectionRepository to check findAll() method

---
+++ src/test/java/com/janflpk/collectionsmanager/repository/FilmsCollectionRepositoryTest.java
@@ -0,0 +1,35 @@
+package com.janflpk.collectionsmanager.repository;
+
+import com.janflpk.collectionsmanager.backend.domain.films.FilmsCollection;
+import com.janflpk.collectionsmanager.backend.repository.FilmsCollectionRepository;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FilmsCollectionRepositoryTest {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(FilmsCollectionRepositoryTest.class);
+
+    @Autowired
+    private FilmsCollectionRepository filmsCollectionRepository;
+
+    @Test
+    private void shouldFetchAllFilmsCollectionTest() {
+        //Given & When
+        List<FilmsCollection> allFilmsCollections = filmsCollectionRepository.findAll();
+
+        //Then
+        LOGGER.info("Lista wszystkich kolekcji filmów = " + allFilmsCollections.size());
+        Assert.assertNotNull(allFilmsCollections);
+        Assert.assertNotEquals(0, allFilmsCollections);
+    }
+}
