07dd3af78f553676938f527afde3f7b24cb0e12a
J-Fi <y.neczek@gmail.com>
2020-05-25 23:43:37 +0200

    add list of books collections with names only

--- src/main/java/com/janflpk/collectionsmanager/ui/view/BooksCollectionsListView.java
+++ src/main/java/com/janflpk/collectionsmanager/ui/view/BooksCollectionsListView.java
@@ -1,6 +1,7 @@
 package com.janflpk.collectionsmanager.ui.view;
 
 import com.janflpk.collectionsmanager.backend.domain.books.BooksCollection;
+import com.janflpk.collectionsmanager.backend.service.BooksCollectionDbService;
 import com.janflpk.collectionsmanager.ui.MainLayout;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
@@ -9,18 +10,26 @@ import com.vaadin.flow.router.Route;
 @Route(value = "books-collections", layout = MainLayout.class)
 public class BooksCollectionsListView extends VerticalLayout {
 
+    private BooksCollectionDbService booksCollectionDbService;
+
     Grid<BooksCollection> booksCollectionGrid = new Grid<>(BooksCollection.class);
 
-    public BooksCollectionsListView() {
+    public BooksCollectionsListView(BooksCollectionDbService booksCollectionDbService) {
+        this.booksCollectionDbService = booksCollectionDbService;
 
-
-
+        configureBooksCollectionGrid();
+        updateBooksCollectionsList();
         add(booksCollectionGrid);
+        setSizeFull();
     }
 
     private void configureBooksCollectionGrid() {
         booksCollectionGrid.setSizeFull();
-        booksCollectionGrid.setColumns("collectionName")
-        booksCollectionGrid.addColumn(), "Number of books");
+        booksCollectionGrid.setColumns("collectionName");
+        //booksCollectionGrid.addColumn(e -> e.getBooks()).setHeader("Number of books");
+    }
+
+    private void updateBooksCollectionsList() {
+        booksCollectionGrid.setItems(booksCollectionDbService.findAll());
     }
 }
