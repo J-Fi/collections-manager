4f7c676ac3638aadb7f611af5a5a80d77fcc7c02
J-Fi <y.neczek@gmail.com>
2020-05-30 21:57:18 +0200

    modify header's text

--- src/main/java/com/janflpk/collectionsmanager/ui/view/HomeView.java
+++ src/main/java/com/janflpk/collectionsmanager/ui/view/HomeView.java
@@ -1,9 +1,19 @@
 package com.janflpk.collectionsmanager.ui.view;
 
 import com.janflpk.collectionsmanager.ui.MainLayout;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
 @Route(value = "", layout = MainLayout.class)
 public class HomeView extends VerticalLayout {
+
+    H1 welcomeTextHeader = new H1("Witaj!");
+    H2 welcomeTextBody = new H2(" Aplikacja do zarządzania Twoimi kolekcjami czeka na Twoje rozkazy!");
+
+    public HomeView() {
+
+        add(welcomeTextHeader, welcomeTextBody);
+    }
 }
