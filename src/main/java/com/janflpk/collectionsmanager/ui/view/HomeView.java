1278bd5c8cee13cc2fb0768a3af34463087cb265
J-Fi <y.neczek@gmail.com>
2020-05-30 21:04:04 +0200

    add home view

---
+++ src/main/java/com/janflpk/collectionsmanager/ui/view/HomeView.java
@@ -0,0 +1,8 @@
+package com.janflpk.collectionsmanager.ui.view;
+
+import com.janflpk.collectionsmanager.ui.MainLayout;
+import com.vaadin.flow.router.Route;
+
+@Route(value = "", layout = MainLayout.class)
+public class HomeView {
+}
