a86e7af782a8453f755d7136b690b79468c86157
J-Fi <y.neczek@gmail.com>
2020-05-31 19:24:46 +0200

    add CRUD buttons to BooksCollectionsListView.class

--- src/main/java/com/janflpk/collectionsmanager/ui/view/BooksCollectionsListView.java
+++ src/main/java/com/janflpk/collectionsmanager/ui/view/BooksCollectionsListView.java
@@ -3,6 +3,7 @@ package com.janflpk.collectionsmanager.ui.view;
 import com.janflpk.collectionsmanager.backend.domain.books.BooksCollection;
 import com.janflpk.collectionsmanager.backend.service.BooksCollectionDbService;
 import com.janflpk.collectionsmanager.ui.MainLayout;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
@@ -15,6 +16,10 @@ import java.util.Map;
 @Route(value = "books-collections", layout = MainLayout.class)
 public class BooksCollectionsListView extends VerticalLayout {
 
+    private Button addNewCollection = new Button("Zapisz");
+    private Button updateCollection = new Button("Edytuj");
+    private Button deleteCollection = new Button("Usuń");
+
     private BooksCollectionDbService booksCollectionDbService;
 
     Grid<BooksCollection> booksCollectionGrid;
@@ -26,7 +31,7 @@ public class BooksCollectionsListView extends VerticalLayout {
 
         configureBooksCollectionGrid();
         updateBooksCollectionsList();
-        add(collectionsGridHeader, booksCollectionGrid);
+        add(collectionsGridHeader, addNewCollection, booksCollectionGrid);
         setSizeFull();
     }
 
