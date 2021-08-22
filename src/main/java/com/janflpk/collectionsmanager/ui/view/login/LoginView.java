87efdd748b06c5fa71607d74b8264b8e642091bf
Jan Filipek <janflpk@yahoo.com>
2021-08-22 13:07:27 +0200

    add LoginView

---
+++ src/main/java/com/janflpk/collectionsmanager/ui/view/login/LoginView.java
@@ -0,0 +1,40 @@
+package com.janflpk.collectionsmanager.ui.view.login;
+
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.login.LoginForm;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.BeforeEnterEvent;
+import com.vaadin.flow.router.BeforeEnterObserver;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+
+import java.util.Collections;
+
+@Route("login")
+@PageTitle("Collections Manager | Login")
+public class LoginView extends VerticalLayout implements BeforeEnterObserver {
+
+    private LoginForm login = new LoginForm();
+
+    public LoginView() {
+        addClassName("login-view");
+        setSizeFull();
+        setAlignItems(Alignment.CENTER);
+        setJustifyContentMode(JustifyContentMode.CENTER);
+
+        login.setAction("login");
+
+        add(new H1("Collections manager"), login);
+    }
+
+    @Override
+    public void beforeEnter(BeforeEnterEvent event) {
+        if(!event.getLocation()
+                .getQueryParameters()
+                .getParameters()
+                .getOrDefault("error", Collections.emptyList())
+                .isEmpty()) {
+            login.setError(true);
+        }
+    }
+}
