26133794d9f5e3be55f6a605d8579104100588bb
Jan Filipek <janflpk@yahoo.com>
2021-09-26 06:38:22 +0200

    modify ConfigureUIServieInitListener.java to prevent rerouting unlogged users to LoginView when accessing RegistrationView

--- src/main/java/com/janflpk/collectionsmanager/backend/security/ConfigureUIServiceInitListener.java
+++ src/main/java/com/janflpk/collectionsmanager/backend/security/ConfigureUIServiceInitListener.java
@@ -1,6 +1,7 @@
 package com.janflpk.collectionsmanager.backend.security;
 
 import com.janflpk.collectionsmanager.ui.view.login.LoginView;
+import com.janflpk.collectionsmanager.ui.view.register.RegisterView;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.router.BeforeEnterEvent;
 import com.vaadin.flow.server.ServiceInitEvent;
@@ -18,9 +19,16 @@ public class ConfigureUIServiceInitListener implements VaadinServiceInitListener
         });
     }
 
+    /*
+    Reroute to LoginView if the accessed page is not LoginView and RegisterView.
+    Thanks to this it is possible to access RegisterView for users not logged in.
+    Otherwise unlogged users will be rerouted to LoginView even if they try to
+    access register form.
+     */
     private void authenticateNavigation(BeforeEnterEvent event) {
         if (!LoginView.class.equals(event.getNavigationTarget())
-            && !SecurityUtils.isUserLoggedIn()) {
+            && !RegisterView.class.equals(event.getNavigationTarget())
+                && !SecurityUtils.isUserLoggedIn()) {
             event.rerouteTo(LoginView.class);
         }
     }
