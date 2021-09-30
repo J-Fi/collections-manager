4ba34ed42572b2a9eccf04bfe806ed58db071dcb
Jan Filipek <janflpk@yahoo.com>
2021-09-30 23:30:06 +0200

    write body of UserDetailsImpl and UserDetailsServiceImpl classes

--- src/main/java/com/janflpk/collectionsmanager/backend/security/UserDetailsImpl.java
+++ src/main/java/com/janflpk/collectionsmanager/backend/security/UserDetailsImpl.java
@@ -1,2 +1,56 @@
-package com.janflpk.collectionsmanager.backend.security;public class UserDetailsImpl {
+package com.janflpk.collectionsmanager.backend.security;
+
+import com.janflpk.collectionsmanager.backend.domain.user.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class UserDetailsImpl implements UserDetails {
+
+    private User user;
+
+    public UserDetailsImpl(User user) {
+        this.user = user;
+    }
+
+    public Long getUsernameId() {
+        return user.getUserId();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public Collection<GrantedAuthority> getAuthorities() {
+        return new ArrayList<>();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    };
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    };
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    };
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    };
 }
--- src/main/java/com/janflpk/collectionsmanager/backend/service/UserDetailsServiceImpl.java
+++ src/main/java/com/janflpk/collectionsmanager/backend/service/UserDetailsServiceImpl.java
@@ -1,2 +1,26 @@
-package com.janflpk.collectionsmanager.backend.service;public class UserDetailsServiceImpl {
+package com.janflpk.collectionsmanager.backend.service;
+
+import com.janflpk.collectionsmanager.backend.domain.user.User;
+import com.janflpk.collectionsmanager.backend.repository.UserRepository;
+import com.janflpk.collectionsmanager.backend.security.UserDetailsImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String userEmail) {
+        User user = userRepository.getUserByEmail(userEmail);
+        if (user == null) {
+            throw new UsernameNotFoundException(userEmail);
+        }
+        return new UserDetailsImpl(user);
+    }
 }
