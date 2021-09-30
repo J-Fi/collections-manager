05b4d9e1ccbcb86ef66205715345b218da9af03b
Jan Filipek <janflpk@yahoo.com>
2021-09-30 23:36:12 +0200

    ensure encryption of the password in UserMapper class before loading it to the database

--- src/main/java/com/janflpk/collectionsmanager/backend/mapper/UserMapper.java
+++ src/main/java/com/janflpk/collectionsmanager/backend/mapper/UserMapper.java
@@ -2,15 +2,20 @@ package com.janflpk.collectionsmanager.backend.mapper;
 
 import com.janflpk.collectionsmanager.backend.domain.user.User;
 import com.janflpk.collectionsmanager.backend.domain.user.UserDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserMapper {
 
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
     public User mapToUser(UserDto userDto) {
         return new User(userDto.getFirstName(),
                 userDto.getLastName(),
                 userDto.getEmail(),
-                userDto.getPassword());
+                bCryptPasswordEncoder.encode(userDto.getPassword()));
     }
 }
