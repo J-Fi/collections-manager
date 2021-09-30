a9b8f4ec55f25df3ef01a0386f205c4003a06333
Jan Filipek <janflpk@yahoo.com>
2021-09-30 23:31:07 +0200

    clean the code in RegisterView

--- src/main/java/com/janflpk/collectionsmanager/ui/view/register/RegisterView.java
+++ src/main/java/com/janflpk/collectionsmanager/ui/view/register/RegisterView.java
@@ -1,8 +1,6 @@
 package com.janflpk.collectionsmanager.ui.view.register;
 
 import com.janflpk.collectionsmanager.backend.domain.user.facade.UserFacade;
-import com.janflpk.collectionsmanager.backend.mapper.UserMapper;
-import com.janflpk.collectionsmanager.backend.service.UserDbService;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
